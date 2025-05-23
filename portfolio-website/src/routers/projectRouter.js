fb6c4a016a6327b14b6174efb2e0c2a41db5bc42
Sfi-codes <sfi0220@protonmail.com>
2025-05-23 20:03:56 +0000

    changed frontend router url to new server domain

--- portfolio-website/src/routers/projectRouter.js
+++ portfolio-website/src/routers/projectRouter.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
-
+const url = "https://portfolio-website-backend-ten.vercel.app/api/projects"
 export const getAll = () => {
-	const response = axios.get('/api/projects')
+	const response = axios.get(url)
 
 	return response
 }
